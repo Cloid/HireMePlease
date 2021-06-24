42c41429d4b68d15379cb0667d08eeec1bb895b0
Cloid <nintendogamer@live.com>
2021-06-24 01:29:39 -0700

    Lobby List

--- HireMePlease/Assets/Scripts/Network/LobbyNetworkManager.cs
+++ HireMePlease/Assets/Scripts/Network/LobbyNetworkManager.cs
@@ -8,21 +8,25 @@ using UnityEngine.UI;
 public class LobbyNetworkManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private InputField _roomInput;
+    [SerializeField] private RoomItemUI _roomUIPrefab;
+    [SerializeField] private Transform _roomListParent;
+    private List<RoomItemUI> _roomList = new List<RoomItemUI>();
     // Start is called before the first frame update
     void Start()
     {
       Connect();  
     }
 
-    private void Connect(){
-        PhotonNetwork.NickName = "Player" + Random.Range(0,5000);
-        PhotonNetwork.ConnectUsingSettings();
-        PhotonNetwork.AutomaticallySyncScene = true;
-    }
+    #region PhotonCallbacks
 
     public override void OnConnectedToMaster(){
         Debug.Log("Connected to master server");
         PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        UpdateRoomList(roomList);
     }
 
     public override void OnJoinedLobby()
@@ -34,6 +38,37 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
     {
         Debug.Log("Joined Room: " + PhotonNetwork.CurrentRoom.Name);
         //base.OnJoinedRoom();
+    }
+
+    #endregion
+
+    
+    private void Connect(){
+        PhotonNetwork.NickName = "Player" + Random.Range(0,5000);
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void UpdateRoomList(List<RoomInfo> roomList){
+        //Clear the current list of rooms
+        for(int i = 0; i<_roomList.Count;i++){
+            Destroy(_roomList[i].gameObject);
+        }
+        _roomList.Clear();
+        //Generate a new list with the updated info
+        for(int i = 0; i< roomList.Count;i++){
+            //skip empty rooms
+            if(roomList[i].PlayerCount==0){ continue;}
+            RoomItemUI newRoomItem = Instantiate(_roomUIPrefab);
+            newRoomItem.LobbyNetworkParent = this;
+            newRoomItem.SetName(roomList[i].Name);
+            newRoomItem.transform.SetParent(_roomListParent);
+            _roomList.Add(newRoomItem);
+        }
+    }
+
+    public void JoinRoom(string roomName){
+        PhotonNetwork.JoinRoom(roomName);
     }
 
     public void CreateRoom(){
---
+++ HireMePlease/Assets/Scripts/Network/RoomItemUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomItemUI : MonoBehaviour
+{
+    public LobbyNetworkManager LobbyNetworkParent;
+    [SerializeField] private Text _roomName;
+
+    public void SetName(string roomName){
+        _roomName.text = roomName;
+    }
+
+    public void OnJoinPressed(){
+        LobbyNetworkParent.JoinRoom(_roomName.text);
+    }
+}
