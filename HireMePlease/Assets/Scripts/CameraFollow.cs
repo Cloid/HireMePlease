9d6fe90d130c75072e9fc4710a116dfff7e6a8ef
Josue Armando Uriarte Reyes <47550290+JosueUriarte@users.noreply.github.com>
2021-06-27 00:44:02 -0700

    smoothed camera

--- HireMePlease/Assets/Scripts/CameraFollow.cs
+++ HireMePlease/Assets/Scripts/CameraFollow.cs
@@ -9,10 +9,10 @@ public class CameraFollow : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(target == null){return;}
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, 10));
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
