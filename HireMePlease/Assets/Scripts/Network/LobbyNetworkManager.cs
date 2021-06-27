08d94a88921b257fbc76872ccf37dc901a0d00ef
Cloid <nintendogamer@live.com>
2021-06-26 21:30:26 -0700

    Added Player Name list
    Can see other players in list now!

--- HireMePlease/Assets/Scripts/Network/LobbyNetworkManager.cs
+++ HireMePlease/Assets/Scripts/Network/LobbyNetworkManager.cs
@@ -10,10 +10,18 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
     [SerializeField] private InputField _roomInput;
     [SerializeField] private RoomItemUI _roomUIPrefab;
     [SerializeField] private Transform _roomListParent;
+    [SerializeField] private RoomItemUI _playerItemUIPrefab;
+    [SerializeField] private Transform _playerListParent;
+
+    [SerializeField] private Text _statusField;
+    [SerializeField] private Button _leaveRoomButton;
+    private List<RoomItemUI> _playerList = new List<RoomItemUI>();
+
     private List<RoomItemUI> _roomList = new List<RoomItemUI>();
     // Start is called before the first frame update
     void Start()
     {
+        Initialize();
       Connect();  
     }
 
@@ -36,12 +44,24 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
+        _statusField.text = "Joined " + PhotonNetwork.CurrentRoom.Name;
         Debug.Log("Joined Room: " + PhotonNetwork.CurrentRoom.Name);
-        //base.OnJoinedRoom();
+        _leaveRoomButton.interactable = true;
+        UpdatePlayerList();
+    }
+
+    public override void OnLeftRoom(){
+        _statusField.text = "Lobby";
+        Debug.Log("LeftRoom");
+        _leaveRoomButton.interactable = false;
+        UpdatePlayerList();
     }
 
     #endregion
 
+    private void Initialize(){
+        _leaveRoomButton.interactable = false;
+    }
     
     private void Connect(){
         PhotonNetwork.NickName = "Player" + Random.Range(0,5000);
@@ -67,6 +87,24 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
         }
     }
 
+    private void UpdatePlayerList(){
+        //Clear the current player list
+        for(int i = 0; i<_playerList.Count;i++){
+            Destroy(_playerList[i].gameObject);
+        }
+        _playerList.Clear();
+
+        if(PhotonNetwork.CurrentRoom == null){ return; }
+
+        //Generate a new list of players
+        foreach (KeyValuePair<int, Photon.Realtime.Player> player in PhotonNetwork.CurrentRoom.Players){
+            RoomItemUI newPlayerItem = Instantiate(_playerItemUIPrefab);
+            newPlayerItem.transform.SetParent(_playerListParent);
+            newPlayerItem.SetName(player.Value.NickName);
+            _playerList.Add(newPlayerItem);
+        }
+    }
+
     public void JoinRoom(string roomName){
         PhotonNetwork.JoinRoom(roomName);
     }
@@ -75,5 +113,9 @@ public class LobbyNetworkManager : MonoBehaviourPunCallbacks
         if(string.IsNullOrEmpty(_roomInput.text) == false){
             PhotonNetwork.CreateRoom(_roomInput.text, new RoomOptions() { MaxPlayers = 4}, null);
         }
+    }
+
+    public void LeaveRoom(){
+        PhotonNetwork.LeaveRoom();
     }
 }
