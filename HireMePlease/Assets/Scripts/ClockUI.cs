056c70f2c19185f2f39bde014c6d55e98f820b6a
Modesto Amador <modestoamador16@gmail.com>
2021-06-30 17:00:06 -0700

    leaderboard done and some ui tweeks. sound added

---
+++ HireMePlease/Assets/Scripts/ClockUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockUI : MonoBehaviour {
+    public float timeStart = 10;
+    public Text textBox;
+    public GameObject leaderboard;
+
+    private void Start()
+    {
+        textBox.text = timeStart.ToString();
+    }
+    void Update() {
+        timeStart -= Time.deltaTime;
+        textBox.text = Mathf.Round(timeStart).ToString();
+        if(timeStart <= 0)
+        {
+            //end game
+            textBox.text = "0";
+            leaderboard.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+}
