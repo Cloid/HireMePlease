7f7cb79447c0d72998064ea49b3553fd9e3251cc
Cloid <nintendogamer@live.com>
2021-07-01 16:14:17 -0700

    Leaderboard Position

--- HireMePlease/Assets/Scripts/Player.cs
+++ HireMePlease/Assets/Scripts/Player.cs
@@ -215,5 +215,6 @@ public class Player : MonoBehaviour
 
     public void addPts(){
         taskDone += bonusPt;
+        bonusPt = 0;
     }
 }
---
+++ HireMePlease/Assets/Scripts/SortingLeaderboard.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SortingLeaderboard : MonoBehaviour
+{
+    //References to leaderboard rankings
+    [SerializeField] private GameObject rank1;
+    [SerializeField] private GameObject rank2;
+    [SerializeField] private GameObject rank3;
+    [SerializeField] private GameObject rank4;
+
+    private GameObject[] goList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goList = GameObject.FindGameObjectsWithTag("Player");
+        //Add Bonus Points
+        for(int idx = 0; idx < goList.Length;idx++){
+            goList[idx].GetComponent<Player>().addPts();
+        }
+
+        var newList = goList.OrderBy(e => e.GetComponent<Player>().taskDone).ToList();
+        
+        foreach(GameObject go in newList){
+           int idx = go.GetComponent<Player>().taskDone;
+           Debug.Log("idx");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
